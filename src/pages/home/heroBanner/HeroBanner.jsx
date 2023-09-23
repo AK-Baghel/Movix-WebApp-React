8568e99ada643365279570f9cad18e94c955d65d
Ankit BagheL <ak9664773@gmail.com>
2023-09-23 18:14:33 +0530

    fetching api, add react-router, built heroBAnner component

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import "./style.scss"
+import { useNavigate } from 'react-router-dom';
+import useFetch from '../../../hooks/useFetch';
+
+function HeroBanner() {
+
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { data, loading } = useFetch('/movie/upcoming');
+
+    useEffect(() => {
+        const bg = data?.results[Math.floor(Math.random() * 20)].backdrop_path;
+        setBackground(bg);
+    }, [data]);
+
+    const searchQueryHandler = (event) => {
+        if (event.key === 'Enter' && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    }
+
+    return (
+        <div className="heroBanner">
+            <div className="wrapper">
+                <div className="heroBannerContent">
+                    <span className="title">
+                        <span className="subTitle">
+                            Millions of movies, TV shows and people to discover.
+                            Explore now.
+                        </span>
+                        <div className="searchInput">
+                            <input type="text" placeholder='Search for a movie or tv show...' onChange={(e) => { setQuery(e.target.value) }} onKeyUp={searchQueryHandler} />
+                            <button>Search</button>
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroBanner
