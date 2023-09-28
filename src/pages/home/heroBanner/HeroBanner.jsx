558566a764b28b430afd87f3acbeb5328320addf
Ankit BagheL <ak9664773@gmail.com>
2023-09-28 16:26:18 +0530

    completed home-banner section with it's css

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react'
 import "./style.scss"
 import { useNavigate } from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
+import { useSelector } from 'react-redux';
+import Img from "../../../components/lazyLoadImage/Img"
+import ContentWrapper from '../../../components/contentWrapper/contentWrapper';
 
 function HeroBanner() {
 
     const [background, setBackground] = useState("");
     const [query, setQuery] = useState("");
     const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home)
     const { data, loading } = useFetch('/movie/upcoming');
 
     useEffect(() => {
-        const bg = data?.results[Math.floor(Math.random() * 20)].backdrop_path;
+        const bg = url.backdrop + data?.results[Math.floor(Math.random() * 20)].backdrop_path;
         setBackground(bg);
     }, [data]);
 
@@ -23,9 +27,16 @@ function HeroBanner() {
 
     return (
         <div className="heroBanner">
-            <div className="wrapper">
-                <div className="heroBannerContent">
-                    <span className="title">
+            {!loading && <div className="backdrop-img">
+                <span className='lazy-load-image-background blur lazy-load-image-loaded' style={{ color: "transparent", display: "inline-block" }}>
+                    <img src={background} alt="" />
+                </span>
+            </div>}
+            <div className="opacity-layer"></div>
+            <ContentWrapper>
+                <div className="wrapper">
+                    <div className="heroBannerContent">
+                        <span className="title">Welcome</span>
                         <span className="subTitle">
                             Millions of movies, TV shows and people to discover.
                             Explore now.
@@ -34,9 +45,10 @@ function HeroBanner() {
                             <input type="text" placeholder='Search for a movie or tv show...' onChange={(e) => { setQuery(e.target.value) }} onKeyUp={searchQueryHandler} />
                             <button>Search</button>
                         </div>
-                    </span>
+
+                    </div>
                 </div>
-            </div>
+            </ContentWrapper>
         </div>
     )
 }
